280dc434457d91c6c2bd88004cb996f922c9ade9
Julian Guillo <julianleioa@gmail.com>
2025-04-21 13:45:10 +0200

    Added first draft of layout and basic table

--- app.R
+++ app.R
@@ -1,52 +1,95 @@
-library(shiny)
-library(arrow)
-library(dplyr)
-library(DT)
+# Load global config
+source("global.R")
 
-# Load species names from RDS
-species_names <- readRDS("data/poland_species_names.rds")
-
-# Define UI
-ui <- fluidPage(
-  titlePanel("Poland Species Observations"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "species",
-        label = "Select Scientific Name",
-        choices = species_names,
-        selected = NULL,
-        multiple = FALSE,
-        selectize = TRUE
-      )
+ui <- page_navbar(
+  title = "Species Observations",
+  fillable = TRUE,
+  id = "view_mode",
+  
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "main.css")
+  ),
+  
+  nav_panel("Per country"),
+  nav_panel("Europe"),
+  
+  layout_columns(
+    col_widths = c(7, 5),
+    
+    div(
+      class = "m-3",
+      card(class = "mb-3 map", "testing"),
+      card(class = "timeline", style = "min-height: 20vh;", "This is a small card, e.g., for summary or status.")
     ),
-    mainPanel(
+    
+    div(
+      class = "m-3",
+      mod_search_ui("search"),
       DTOutput("data_table")
     )
   )
 )
 
-# Define server
 server <- function(input, output, session) {
+  search_vals <- mod_search_server("search")
   
-  # Reactive expression to filter dataset by selected species
-  filtered_data <- reactive({
-    req(input$species)
+  mock_data <- data.frame(
+    scientificName = "R Shiny Developer",
+    vernacularName = "Julián",
+    longitudeDecimal = -0.4,
+    latitudeDecimal = 39.3,
+    locality = "Albal, Valencia, Spain",
+    countryCode = "ES",
+    eventDate = "2025-04-21",
+    Identifier = "R Shiny Developer",
+    stringsAsFactors = FALSE
+  )
+  
+  filtered_data <- reactiveVal(mock_data)
+  
+  observe({
+    req(search_vals$species() != "")
     
-    # Open just the partition corresponding to the selected species
-    # this is faster than scanning and filtering all partitions
-    path <- file.path(paste0("data/poland_with_multimedia.parquet/scientificName=", URLencode(input$species)))
-    dataset <- open_dataset(path) %>%
-      collect()
-    dataset$scientificName <- input$species # add back the scientific name
-    dataset
+    path <- file.path(paste0("data/europe_data.parquet/", search_vals$country(), ".parquet/scientificName=", URLencode(search_vals$species())))
+    
+    new_data <- tryCatch({
+      dataset <- open_dataset(path) %>% collect()
+      dataset$scientificName <- search_vals$species()
+      dataset
+    }, error = function(e) {
+      data.frame(scientificName = character(0), stringsAsFactors = FALSE)
+    })
+    
+    filtered_data(new_data)
   })
   
-  # Render the datatable
   output$data_table <- renderDT({
-    datatable(filtered_data(), options = list(pageLength = 10), rownames = FALSE)
+    data <- filtered_data()
+    
+    if (nrow(data) == 0) {
+      return(datatable(
+        data.frame(Message = "No observations found for this species in the selected country."),
+        options = list(dom = 't'),
+        rownames = FALSE,
+        class = 'compact small'
+      ))
+    }
+    
+    datatable(
+      data,
+      options = list(
+        pageLength = 10,
+        scrollY = "50vh",
+        scrollX = TRUE,
+        scroller = TRUE,
+        paging = TRUE,
+        dom = 'frtip'
+      ),
+      selection = list(mode = "single", target = "row"),
+      rownames = FALSE,
+      class = 'compact small'
+    )
   })
 }
 
-# Run the app
 shinyApp(ui = ui, server = server)
